514d39e4a79402366811d9defb297629603a2f7b
Angel Ghuge <angelghuge@gmail.com>
2026-05-26 16:06:20 +0530

    Add files via upload

---
+++ disease_prediction.py
@@ -0,0 +1,6 @@
+print("AI Healthcare Disease Prediction System")
+
+symptoms = input("Enter symptoms: ")
+
+print("Analyzing symptoms...")
+print("Possible disease prediction generated successfully.")
